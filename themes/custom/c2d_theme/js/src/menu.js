5864dc634e49833ca454e76575cb2546a7f26ee2
Barabas Csaba <info@barabascsaba.ro>
2021-09-02 08:27:19 +0300

    C2D-9 | Expanded menu item fixed.

--- themes/custom/c2d_theme/js/src/menu.js
+++ themes/custom/c2d_theme/js/src/menu.js
@@ -7,11 +7,24 @@
         let hamburgerIcon = $(context).find('.hamburger');
         let closeMenuIcon = $(context).find('.close-menu');
         let mainMenu = $(context).find('#block-mainnavigation');
+        let expandableItem = $(context).find('.menu-item--expanded span');
+        console.log(expandableItem);
         hamburgerIcon.on('click', function () {
           mainMenu.addClass('open');
         });
         closeMenuIcon.on('click', function () {
           mainMenu.removeClass('open');
+        });
+        expandableItem.on('click', function (e) {
+          let itemParent = $(e.target).parent();
+          console.log(itemParent);
+          // closeSearch();
+          if (itemParent.hasClass('opened')) {
+            itemParent.removeClass('opened');
+          } else {
+            expandableItem.parent().removeClass('opened')
+            itemParent.addClass('opened');
+          }
         });
       }
     }
