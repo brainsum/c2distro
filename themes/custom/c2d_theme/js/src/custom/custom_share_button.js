2f010110c9016c8726133b3fc5380603356aeb90
Barabas Csaba <info@barabascsaba.ro>
2021-09-21 22:44:12 +0300

    SLW-40 | End of the lines fixed.

--- themes/custom/c2d_theme/js/src/custom/custom_share_button.js
+++ themes/custom/c2d_theme/js/src/custom/custom_share_button.js
@@ -1,11 +1,11 @@
 (function shareButton($, Drupal) {
-    Drupal.behaviors.sharebutton = {
-      attach: function (context) {
-        if ($(context).is(document)) {
-            $('.share-button').on('click', function(){
-                $('.rrssb').toggleClass('rrssb-show');
-            })
-        }
+  Drupal.behaviors.sharebutton = {
+    attach: function (context) {
+      if ($(context).is(document)) {
+          $('.share-button').on('click', function(){
+              $('.rrssb').toggleClass('rrssb-show');
+          })
       }
-    };
-  })(jQuery, Drupal);
+    }
+  };
+})(jQuery, Drupal);
