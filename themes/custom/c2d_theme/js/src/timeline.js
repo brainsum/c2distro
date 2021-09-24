1091812c5ea948e99d3b102c27847d3755f5485c
Barabas Csaba <info@barabascsaba.ro>
2021-09-24 13:34:45 +0300

    SLW-34 | console log-s removed from different js files.

--- themes/custom/c2d_theme/js/src/timeline.js
+++ themes/custom/c2d_theme/js/src/timeline.js
@@ -7,8 +7,6 @@
       let closePopup = $('.popup-text--close', context);
       popupTitle.on('click', function (event) {
         let target = $(event.target);
-        console.log($(event.target));
-        console.log($(`div[data-item-id="${target.attr('data-item-id').replace('title', 'popup')}"]`));
         $(`div[data-item-id="${target.attr('data-item-id').replace('title', 'popup')}"]`).addClass('open');
       });
       closePopup.on('click', function () {
